96baa83128539c1ce152f694192d9641584f9ab3
AndreiKachur <underfil@mail.ru>
2022-02-17 08:22:50 +0300

    Fix: set scroll to top PDP while mounting

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import { USER_ROLE } from '../../helpers/constants/constants';
 import withAuthExpiration from '../../HOCs/withAuthExpiration';
@@ -20,41 +20,54 @@ import AuthExpirationModal from '../AuthExpirationModal/AuthExpirationModal';
 import { Layout } from '../Layout/Layout';
 import { ProtectedRoutes } from '../ProtectedRoutes/ProtectedRoutes';
 
-const App = ({ handleClose, isOpenModal }) => (
-  <Layout>
-    <Routes>
-      <Route exact path="/products/:id" element={<ProductDetailsPage />} />
-      <Route exact path="/products" element={<ProductListPage />} />
-      <Route exact path="/cart" element={<CartPage />} />
-      <Route path="/users/:id" element={<ProfilePage />} />
-      <Route path="/profile" element={<ProfilePrivatePage />} />
-      <Route exact path="/" element={<HomePage />} />
-      <Route element={<ProtectedRoutes permissionLevel={USER_ROLE.CONSUMER} />}>
-        <Route exact path="/profile/wishlist" element={<WishListPage />} />
-      </Route>
-      <Route path="/" element={<ProtectedRoutes />}>
+const App = ({ handleClose, isOpenModal }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return (
+    <Layout>
+      <Routes>
+        <Route exact path="/products/:id" element={<ProductDetailsPage />} />
+        <Route exact path="/products" element={<ProductListPage />} />
+        <Route exact path="/cart" element={<CartPage />} />
+        <Route path="/users/:id" element={<ProfilePage />} />
+        <Route path="/profile" element={<ProfilePrivatePage />} />
+        <Route exact path="/" element={<HomePage />} />
         <Route
-          path="/admin/products/create"
-          element={<CreateOrEditProductPage />}
-        />
+          element={<ProtectedRoutes permissionLevel={USER_ROLE.CONSUMER} />}
+        >
+          <Route exact path="/profile/wishlist" element={<WishListPage />} />
+        </Route>
+        <Route path="/" element={<ProtectedRoutes />}>
+          <Route
+            path="/admin/products/create"
+            element={<CreateOrEditProductPage />}
+          />
+          <Route
+            path="/admin/products/:id/edit"
+            element={<CreateOrEditProductPage />}
+          />
+        </Route>
         <Route
-          path="/admin/products/:id/edit"
-          element={<CreateOrEditProductPage />}
-        />
-      </Route>
-      <Route
-        path="/admin"
-        element={<ProtectedRoutes permissionLevel={USER_ROLE.SELLER} />}
-      >
-        <Route path="/admin/products" element={<AdminProductsPage />} />
-        <Route path="/admin/categories" element={<AdminCategoriesPage />} />
-      </Route>
-      <Route path="/profile/address-book" element={<AddressBook />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-    <AuthExpirationModal handleClose={handleClose} isOpenModal={isOpenModal} />
-  </Layout>
-);
+          path="/admin"
+          element={<ProtectedRoutes permissionLevel={USER_ROLE.SELLER} />}
+        >
+          <Route path="/admin/products" element={<AdminProductsPage />} />
+          <Route path="/admin/categories" element={<AdminCategoriesPage />} />
+        </Route>
+        <Route path="/profile/address-book" element={<AddressBook />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      <AuthExpirationModal
+        handleClose={handleClose}
+        isOpenModal={isOpenModal}
+      />
+    </Layout>
+  );
+};
 
 App.propTypes = {
   isOpenModal: PropTypes.bool.isRequired,
